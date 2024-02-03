6e2d01988d29a78c2f192ff5fe12036ae6b0a8a6
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-03 12:07:44 -0300

    atualizado service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,12 +6,12 @@ export const register = () => {
         navigator.serviceWorker
           .register('/service-worker.js')
           .then((registration) => {
-            console.log('Service Worker registrado com sucesso:', registration);
+            console.log('Service Worker registrado com sucesso:', registration)
           })
           .catch((error) => {
-            console.error('Erro ao registrar o Service Worker:', error);
-          });
-      });
+            console.error('Erro ao registrar o Service Worker:', error)
+          })
+      })
     }
-  };
+  }
   
