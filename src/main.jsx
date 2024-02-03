e512d366fc799a78b762b130878f1569129f7417
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-03 14:03:39 -0300

    atualizar sw

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import swDev from "./swDev.js"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-swDev()
