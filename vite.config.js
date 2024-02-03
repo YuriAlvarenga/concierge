6caaaf294ded72e617505058634d919c0be18331
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-03 14:28:11 -0300

    atualiz

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+    icons:[{
+      src: "/512.png",
+      sizes: "512x512",
+      type:"image/png",
+      purpose:"any maskable"
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      manifest: {
-        icons: [
-          {
-            src: "/512.png",
-            sizes: "512x512",
-            type:"image/png",
-            purpose:"any maskable"
-          }
-        ]
-      }
-    })
-  ],
+    VitePWA(manifestForPlugIn)
+  ]
 })
