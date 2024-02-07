1a9148a3438421ad4d3f7d71342f2fe84a7e61c9
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-07 11:07:54 -0300

    criado notifications e categories

--- src/components/menu/menu-bottom/menu-bottom.jsx
+++ src/components/menu/menu-bottom/menu-bottom.jsx
@@ -6,6 +6,7 @@ import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications'
 import PersonIcon from '@mui/icons-material/Person'
 import { styled } from '@mui/system'
 import AddIcon from '@mui/icons-material/Add'
+import { PropaneSharp } from '@mui/icons-material'
 
 const RootBottomNavigation = styled(BottomNavigation)({
   width: '100%',
@@ -16,19 +17,28 @@ const RootBottomNavigation = styled(BottomNavigation)({
   padding:0
 })
 
-export default function MenuBottomNavigation() {
+export default function MenuBottomNavigation({handleItemClick}) {
+
+  //associa o nome e icon que estão sendo clicados
   const [value, setValue] = useState('home')
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
 
+  //lista de itens do menu inferior
+  const itemsMenu = [
+    { id:1, label: "Início", value: "home", icon:<HomeIcon /> },
+    { id:2, label: "Recepção", value: "reception", icon:<PersonIcon /> },
+    { id:3, label: "Notificações", value: "notifications", icon:<CircleNotificationsIcon /> },
+    { id:4, label: "Categorias", value: "categories", icon:<AddIcon /> }
+  ]
+
   return (
-    <RootBottomNavigation value={value} onChange={handleChange}>
-      <BottomNavigationAction label="Início" value="home" icon={<HomeIcon />} sx={{color:"#fff"}} />
-      <BottomNavigationAction label="Recepção" value="reception" icon={<PersonIcon />} sx={{color:"#fff"}} />
-      <BottomNavigationAction label="Notificações" value="notifications" icon={<CircleNotificationsIcon />} sx={{color:"#fff"}} />
-      <BottomNavigationAction label="Categorias" value="categories" icon={<AddIcon />} sx={{color:"#fff"}} />
+    <RootBottomNavigation value={value} onChange={handleChange} >
+      {itemsMenu.map((item) =>(
+        <BottomNavigationAction onClick={()=>{setValue(item.value);handleItemClick(item.id)}} key={item.id} label={item.label} value={item.value} icon={item.icon} sx={{color:"#fff"}} />
+      ))}
     </RootBottomNavigation>
   )
 }
