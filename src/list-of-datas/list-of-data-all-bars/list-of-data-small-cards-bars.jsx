58d40c27b81ff2df263deeed2c963c1f897a8ef4
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-10 14:39:35 -0300

    icons cards

--- src/list-of-datas/list-of-data-all-bars/list-of-data-small-cards-bars.jsx
+++ src/list-of-datas/list-of-data-all-bars/list-of-data-small-cards-bars.jsx
@@ -9,7 +9,7 @@ const dataOfSmallCardsFromBars =
         { id: 7, title: 'Sertanejo', icon: <MusicNoteIcon sx={{color:'#FFF'}}/>, gradient: 'linear-gradient(to bottom, #ff6888, #ff6888)' },
         { id: 8, title: 'Brasilidade', icon: <MusicNoteIcon sx={{color:'#FFA500'}}/>, gradient: 'linear-gradient(to bottom, green 50%, #FFA500)' },
         { id: 5, title: 'Hip-Hop', icon: <MusicNoteIcon sx={{color:'#FFF'}}/>, gradient: 'linear-gradient(to bottom, #b07eb9, #b07eb9)' },
-        { id: 4, title: 'Rock', icon: <GiGuitar sx={{color:'#FFF'}}/>, gradient: 'linear-gradient(to bottom, #000000 60%, #AAAAAA)' },
+        { id: 4, title: 'Rock', icon: <MusicNoteIcon sx={{color:'#FFF'}}/>, gradient: 'linear-gradient(to bottom, #000000 60%, #AAAAAA)' },
         { id: 2, title: 'Sunset', icon: <WbTwilightIcon sx={{color:'#FFD700'}}/>, gradient: 'linear-gradient(to bottom, #FFA500 60%, #1fe4f5 )'},  
         { id: 6, title: 'Eletrônica', icon: <MusicNoteIcon sx={{color:'#ff6888'}}/>, gradient: 'linear-gradient(to bottom, #FF69B4, #b07eb9 55%, #1fe4f5 )' },
     ]
