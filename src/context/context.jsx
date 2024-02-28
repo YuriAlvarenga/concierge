7a25cb8984dcfe56adcb8f95b9488ecfa3b7b192
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-28 13:34:15 -0300

    adicionado nova logica em context

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,15 +1,43 @@
-import React, { createContext, useContext, useState } from "react"
+import React, { createContext, useContext, useState, useEffect } from "react"
+import { hotels } from "../list-of-datas/list-of-data-hotel-page/list-of-hotels"
+import { useParams } from "react-router-dom"
 
-export const HotelContext = createContext()
+export const HotelContext = createContext();
 
 export const HotelProvider = ({ children }) => {
   const [selectedHotel, setSelectedHotel] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    // Verificar se há um ID na URL
+    if (id) {
+      // Encontrar o hotel com base no ID da URL
+      const foundHotel = hotels.find((hotel) => hotel.id === parseInt(id))
+      // Definir o hotel selecionado com base no ID da URL
+      setSelectedHotel(foundHotel)
+      // Armazenar o ID no localStorage
+      localStorage.setItem("selectedHotelId", id)
+    }
+  }, [id])
+
+  // Verificar se o hotel selecionado não foi definido e há um ID armazenado no localStorage
+  useEffect(() => {
+    if (!selectedHotel) {
+      const storedHotelId = localStorage.getItem("selectedHotelId")
+      if (storedHotelId) {
+        // Encontrar o hotel com base no ID armazenado no localStorage
+        const foundHotel = hotels.find(
+          (hotel) => hotel.id === parseInt(storedHotelId)
+        )
+        // Definir o hotel selecionado com base no ID armazenado no localStorage
+        setSelectedHotel(foundHotel)
+      }
+    }
+  }, [selectedHotel])
 
   return (
-    <HotelContext.Provider value={{ selectedHotel, setSelectedHotel }}>
+    <HotelContext.Provider value={{ selectedHotel }}>
       {children}
     </HotelContext.Provider>
   )
 }
-
-
--- src/pages/hotel/hotel-page.jsx
+++ src/pages/hotel/hotel-page.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect} from "react"
-import { useParams } from "react-router-dom"
+import React, { useContext, useEffect} from "react"
 import MenuBottomNavigation from "../../components/menu/menu-bottom/menu-bottom"
 import { Toolbar, Box } from "@mui/material"
 import WifiCard from "../../components/cards/hotel-card/wi-fi-card"
@@ -9,48 +8,35 @@ import ServicesFromHotelCard from "../../components/cards/hotel-card/cards-servi
 import ImageCardFromHotel from "../../components/cards/hotel-card/image-card-from-hotel"
 import HourCard from "../../components/cards/hotel-card/hour-card-hotel"
 import SocialMedia from "../../components/cards/shared-components/card-social-media/social-media"
-import { hotels } from "../../list-of-datas/list-of-data-hotel-page/list-of-hotels"
+import { HotelContext } from "../../context/context"
+
+
 
 export default function HotelPage(){
-    const { id } = useParams()
-    const [hotel, setHotel] = useState(null)
+    const { selectedHotel } = useContext(HotelContext)
 
     useEffect(() => {
-        // Encontrar o hotel com o ID correspondente
-        const selectedHotel = hotels.find((hotel) => hotel.id === parseInt(id))
-    
-        // Verificar se o hotel foi encontrado
-        if (selectedHotel) {
-          setHotel(selectedHotel)
-          localStorage.setItem("selectedHotelId", id)
-        }
-      }, [id])
+        // Aqui você pode adicionar qualquer lógica específica para a página do hotel, se necessário
+        console.log("Hotel selecionado:", selectedHotel)
+      }, [selectedHotel])
 
-    useEffect(() => {
-        const storedHotelId = localStorage.getItem("selectedHotelId")
-        if (storedHotelId && storedHotelId !== id) {
-          // Se o ID do hotel recuperado do localStorage for diferente do ID na URL,
-          // redirecionar para a página com o ID correto
-          window.location.href = `/${storedHotelId}`
-        }
-    }, [id])
 
 
     return(
         <>
-          {hotel && (
+          {selectedHotel  && (
                 <>
-                    <ImageCardFromHotel hotelName={hotel.name} />
+                    <ImageCardFromHotel hotelName={selectedHotel.name} />
                     <Box sx={{ position: "absolute", top: 240, right: 0 }}>
                         <SocialMedia
                             instagramUrl={"https://www.instagram.com/ibiscriciuma/"}
                             facebookUrl={"https://www.facebook.com/hotelibiscriciuma/"}
                         />
                     </Box>
-                    {hotel.services && (
+                    {selectedHotel .services && (
                         <>
                             <Title title={"Serviços"} />
-                            <SmallCard hotelSmallServices={hotel.services} />
+                            <SmallCard hotelSmallServices={selectedHotel.services} />
                         </>
                     )}
                     <WifiCard />
@@ -58,7 +44,7 @@ export default function HotelPage(){
                     <Title title={"Serviços"} />
                     <ServicesFromHotelCard />
                     <Toolbar />
-                    <MenuBottomNavigation hotelId={hotel.id} />
+                    <MenuBottomNavigation hotelId={selectedHotel.id} />
                 </>
             )}
         </>
