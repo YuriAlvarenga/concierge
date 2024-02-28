8e00da96c2894051fe35786eaf2abff3f143f44d
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-02-28 12:53:29 -0300

    ajustado div image home

--- src/components/cards/hotel-card/image-card-from-hotel.jsx
+++ src/components/cards/hotel-card/image-card-from-hotel.jsx
@@ -31,7 +31,7 @@ export default function ImageCardFromHotel({hotelName}) {
 
   return (
     <Box sx={{mb: 1, overflow: 'hidden' }}>
-      <Typography variant="h6" sx={{ position: 'absolute', width: '86%',top:'5%', zIndex:9998, margin:2, color: '#19647e', backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '12px 16px', borderRadius: '4px'}}><ButtonTranslate color="#19647e" />{hotelName}</Typography>
+      <Typography variant="h6" sx={{ position: 'absolute', width: '86%',top:'3%', zIndex:9998, margin:2, color: '#19647e', backgroundColor: 'rgba(255, 255, 255, 0.7)', padding: '12px 16px', borderRadius: '4px'}}><ButtonTranslate color="#19647e" />{hotelName}</Typography>
       <Slider {...settings}>
         {images.map((item, index) => (
           <Box key={item.title} sx={{ position: 'relative', textAlign: 'center', margin: 0, padding: 0 }}>
