59541990303ac965fcfcd05fd0918da7ce5f769f
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-04-11 23:01:16 -0300

    remoção do menu bottom de hotel

--- src/pages/hotel/hotel-page.jsx
+++ src/pages/hotel/hotel-page.jsx
@@ -31,7 +31,6 @@ export default function HotelPage(){
                     <HourCard />
                     <ServicesFromHotelCard />
                     <Toolbar />
-                    <MenuBottomNavigation/>
                 </>
             )}
         </>
