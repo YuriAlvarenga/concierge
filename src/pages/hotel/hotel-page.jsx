4f0161184c08977ebeca4161e1cdc8a36834b912
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-04-11 23:34:20 -0300

    atualizado 11-04-24

--- src/pages/hotel/hotel-page.jsx
+++ src/pages/hotel/hotel-page.jsx
@@ -1,26 +1,28 @@
-import React, { useContext, useState, useEffect} from "react"
-
-import MenuBottomNavigation from "../../components/menu/menu-bottom/menu-bottom"
-import { Toolbar, Box } from "@mui/material"
+import React, { useContext, useEffect } from "react"
+import { Box } from "@mui/material"
 import WifiCard from "../../components/cards/hotel-card/wi-fi-card"
 import SmallCard from "../../components/cards/shared-components/small-cards-components/small-card-from-services"
-import Title from "../../components/cards/shared-components/titles/title"
 import ServicesFromHotelCard from "../../components/cards/hotel-card/cards-services-from-hotel"
 import ImageCardFromHotel from "../../components/cards/hotel-card/image-card-from-hotel"
 import HourCard from "../../components/cards/hotel-card/hour-card-hotel"
 import SocialMedia from "../../components/cards/shared-components/card-social-media/social-media"
 import { HotelContext } from "../../context/context"
+import MenuBottomNavigation from "../../components/menu/menu-bottom/menu-bottom"
 
 export default function HotelPage(){
 
-    const {hotel} = useContext(HotelContext)
+    const { hotel } = useContext(HotelContext)
 
-    return(
-        <>
-          {hotel && (
+    useEffect(() => {
+        // Renderizar apenas quando o hotel estiver disponível
+    }, [hotel])
+
+    return (
+        <Box sx={{ overflowX: 'hidden' }}>
+            {hotel && (
                 <>
                     <ImageCardFromHotel hotelName={hotel.nameHotel} />
-                    <Box sx={{ position: "absolute", top: 240}}>
+                    <Box sx={{ position: "absolute", top: 240, right: 0 }}>
                         <SocialMedia
                             instagramUrl={"https://www.instagram.com/ibiscriciuma/"}
                             facebookUrl={"https://www.facebook.com/hotelibiscriciuma/"}
@@ -30,10 +32,9 @@ export default function HotelPage(){
                     <WifiCard />
                     <HourCard />
                     <ServicesFromHotelCard />
-                    <Toolbar />
                     <MenuBottomNavigation/>
                 </>
             )}
-        </>
+        </Box>
     )
 }
