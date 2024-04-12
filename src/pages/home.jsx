ec9360ae572d80a9eddfaf24c74b29a4b76ee338
YuriAlvarenga <alvarenga.yuri@hotmail.com>
2024-04-12 12:27:28 -0300

    removed fade from page home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,44 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Box, Typography, Fade  } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 
 
-export default function Home(){
+export default function Home() {
   const navigate = useNavigate()
   const [showWelcomeMessage, setShowWelcomeMessage] = useState(true)
 
   useEffect(() => {
     const storedHotelId = localStorage.getItem("selectedHotelId")
     setShowWelcomeMessage(false)
-    setTimeout(()=>{
+    setTimeout(() => {
       if (storedHotelId) {
         navigate(`/${storedHotelId}`)
       }
-    },2000 )
+    }, 2000)
   }, [navigate])
 
   return (
-    <>
-      <Fade in={showWelcomeMessage} timeout={1000}>
-        <Box
-          minHeight="100vh"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          bgcolor="#f3f3f3"
-        >
-          <Box sx={{display:'flex', alignItems:'center', mb: 1}}>
-            <img src="logo-app/concierge-logo.png" alt="Concierge Virtual" style={{ height:80, width:80, borderRadius:10}} />
-            <Typography variant="h6" gutterBottom align="center" sx={{ml:1}}>
-              Concierge Virtual
-            </Typography>
-            <Typography variant="body1" align="center">Redirecionando para sua página...</Typography>
-          </Box>
+    <React.Fragment>
+      <Box minHeight="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" bgcolor="#f3f3f3">
+        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+          <img src="logo-app/concierge-logo.png" alt="Concierge Virtual" style={{ height: 80, width: 80, borderRadius: 10 }} />
+          <Typography variant="h6" gutterBottom align="center" sx={{ ml: 1 }}>
+            Concierge Virtual
+          </Typography>
+          <Typography variant="body1" align="center">Redirecionando para sua página...</Typography>
         </Box>
-      </Fade>
-    </>
+      </Box>
+    </React.Fragment>
   )
 }
 
